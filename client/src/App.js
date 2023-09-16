97ef0701a139d58d0ffb1e2e71e3f457d8557310
Luis Mata <luivader2000@gmail.com>
2023-09-16 06:25:06 -0400

    add auth route

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,53 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { UserProvider, useAuth } from "./contexts/UserContext";
 import HomePage from "./pages/homePage";
 import ProfilePage from "./pages/ProfilePage";
 
+// Fix this when implemented:
+import LoginPage from "./pages/loginPage";
+
+const LOGIN_ROUTE = "/login";
+
+// Only continue to render components if the user is authenticated.
+// Redirect users to login page if not logged in or a choice of a page.
+function RequireAuth({ children }) {
+  let auth = useAuth();
+  if (!auth.user) {
+    console.log("Not logged in, redirecting.")
+    return <Navigate to={LOGIN_ROUTE} />;
+  }
+
+  return children;
+}
+
 function App() {
-  // return <HomePage />;
-  return <ProfilePage />;
+  return (
+    <BrowserRouter>
+      <UserProvider>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <RequireAuth>
+                <HomePage />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <RequireAuth>
+                <ProfilePage />
+              </RequireAuth>
+            }
+          />
+          <Route path={LOGIN_ROUTE} element={<LoginPage />} />
+        </Routes>
+      </UserProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;

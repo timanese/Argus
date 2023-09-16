6158eb4d255cde94c01c61c50abed48b0df92f4a
Samuelmm97 <Sammymenaged@yahoo.com>
2023-09-16 02:14:34 -0400

    added npm start and updated user model to have multiple emergency contacts

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
 
+const EmergencyContactSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+});
+
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  emergencyContact: { type: String },
+  emergencyContacts: [EmergencyContactSchema],
   exchangeHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: "Exchange" }],
 });
 

031e230fd55b3014d71f5e3805f5f9ac61317c97
Samuelmm97 <Sammymenaged@yahoo.com>
2023-09-17 10:27:57 -0400

    remove unique true

--- backend/models/User.js
+++ backend/models/User.js
@@ -10,7 +10,7 @@ const UserSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  phoneNumber: { type: String, required: true, unique: true },
+  phoneNumber: { type: String, required: true },
   location: { type: String },
   profilePicture: { type: mongoose.Schema.Types.ObjectId, ref: "fs.files" },
   driversLicenseFront: {

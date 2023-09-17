c9e21a57f2c46cacde06061641029d9ab9c75097
Samuelmm97 <Sammymenaged@yahoo.com>
2023-09-17 10:38:31 -0400

    remove required true from phonenumber

--- backend/models/User.js
+++ backend/models/User.js
@@ -10,7 +10,7 @@ const UserSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  phoneNumber: { type: String, required: true },
+  phoneNumber: { type: String },
   location: { type: String },
   profilePicture: { type: mongoose.Schema.Types.ObjectId, ref: "fs.files" },
   driversLicenseFront: {
